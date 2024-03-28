7485c04e0a35ff1146fff8e4d553e17ff886bdee
badex-ai <Badamasi.link@gmail.com>
2024-03-28 23:54:01 +0100

    added the hightlights of the event

--- src/Pages/Events/EventPage.js
+++ src/Pages/Events/EventPage.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React,{use} from 'react'
+import Header from "../../components/header";
+
 
 const EventPage = () => {
+
+  const hightlights = [1,2,3,4,5,6,]
+
+  const hight = hightlights.map((hightlight)=>(
+    <div className='w-[30%]'>
+
+    </div>
+  ))
   return (
-    <div>EventPage</div>
+    <div>
+      	<Header 
+				// date={date}
+				title={"Events"}/>
+        <div className='border w-[80%] h-16 '>
+
+        </div>
+        <div className='grid grid-cols-3 '>
+          <p>Watch hightlights</p>
+          <div></div>
+        </div>
+    </div>
   )
 }
 
