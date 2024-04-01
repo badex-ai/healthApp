67284c8118938362de5f8f5a661890ed27cf63fa
badex-ai <Badamasi.link@gmail.com>
2024-04-01 23:45:56 +0100

    added the full portal

--- src/Pages/Events/EventPage.js
+++ src/Pages/Events/EventPage.js
@@ -1,12 +1,21 @@
-import React,{use} from 'react'
+import React,{useState} from 'react'
 import Header from "../../components/header";
 import ButtonSm from '../../components/buttons/buttonSm';
+import FullPortal from '../../components/portal';
 
 
 const EventPage = (props) => {
   const {name}= props
+  const [openPortal, setOpenPortal] = useState(null)
 
   const hightlights = ['1','2','3','4','5','6']
+
+  const playVideo=()=>{
+    console.log('video starts playing')
+    setOpenPortal(true)
+  }
+
+  const video = openPortal ? <FullPortal/> : ''
 
   const videos = hightlights.length ?  hightlights.map((hightlight,index)=>(
     <div className='w-[60%] h-[30rem] border'>
@@ -23,7 +32,7 @@ const EventPage = (props) => {
         </div>
         <div>
         <p>Watch hightlights</p>
-        <ButtonSm text={'+'}/>
+        <ButtonSm click={playVideo} text={'+'}/>
         </div>
         
         <div className='grid grid-cols-3 '>
@@ -32,6 +41,7 @@ const EventPage = (props) => {
             {videos}
          
         </div>
+        {video}
     </div>
   )
 }
---
+++ src/components/portal.js
@@ -0,0 +1,12 @@
+import * as Portal from '@radix-ui/react-portal';
+import React from 'react'
+
+const FullPortal = () => {
+  return (
+    <Portal.Root>Content</Portal.Root>
+  )
+}
+
+export default FullPortal
+
+
