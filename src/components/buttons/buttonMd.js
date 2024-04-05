d7ed2c8e1d36570f3236183205a7e7b0e1cd79bc
badex-ai <Badamasi.link@gmail.com>
2024-04-05 16:19:29 +0100

    added the single image viewer

--- src/components/buttons/buttonMd.js
+++ src/components/buttons/buttonMd.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ButtonMd = ({icon,text},action) => {
     const content = icon ? icon : text
   return (
-    <button onClick={action} className='w-[8rem] h-[3.2rem] rounded-[5rem] bg-black text-2xl flex items-center justify-center'>{content}</button>
+    <button onClick={action} className='cursor-pointer w-[8rem] h-[3.2rem] rounded-[5rem] bg-black text-2xl flex items-center justify-center'>{content}</button>
   )
 }
 
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,15 +1,57 @@
-import React from 'react';
+import React,{useState} from 'react';
 import classes from './profile.module.css'
 import Header from './header';
 import Mystat from './mystat';
 import Imagebox from './imagebox';
 import Stats from './stats';
-
+import Expand from '../icons/expand';
+import FullPortal from './portal';
+import Close from '../assets/icons/close'
 
 
 function Profile() {
+    const [showPortal, setShowPortal] = useState(false)
+    const [closeSinglePicView, setCloseSinglePicView] = useState(false)
     
-     
+     function expandGallery(){
+        setShowPortal(true)
+        console.log('show portal')
+
+     }
+     function showSinglePic(){
+
+     }
+
+     const imgGall = [1,2,3,4,5,6,7,7,8]
+
+     const gridGallery = imgGall.map((img)=>{
+        return <div>
+            {/* <button   > */}
+            <div onClick={showSinglePic}  className='cursor-pointer bg-white h-80 w-80'></div>
+
+            {/* </button> */}
+            {/* <img src="" alt="" /> */}
+        </div>
+     })
+
+     const singlePicViewer = closeSinglePicView ? <div className='z-10 bg-green-200 '>
+        <div onClick={setCloseSinglePicView(true)} >
+            <Close />
+        </div>
+
+
+     </div> : ''
+
+     const portal = showPortal ? <FullPortal>
+        <div className='relative grid grid-cols-5 gap-2 w-[115rem] h-[50rem] p-20' >
+            <div className="absolute top-2 right-2 cursor-pointer" onClick={()=>setShowPortal(false)}>
+            <Close   />
+
+            </div>
+        {gridGallery}
+        </div>
+    
+ </FullPortal> : ''
 
     
     return (
@@ -25,13 +67,24 @@ function Profile() {
             <Imagebox ></Imagebox>
             </div>
            
+           
            <div className={classes.stats}>
            <Stats ></Stats>
            </div>
+           <div className={classes.gallery}>
+                <div  className='relative w-96 bg-green-500 h-80'>
+                    <div className='absolute cursor-pointer top-2 right-2'>
+                        <Expand action={expandGallery} />
+                        </div>
+                </div>
+            </div>
            
            
+            {portal}
+            {singlePicViewer}
           
         </div>
+        
     )
 }
 
