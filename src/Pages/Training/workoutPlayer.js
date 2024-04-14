8818de7fc613ec71235f329d0422f8652bcb04d9
badex-ai <Badamasi.link@gmail.com>
2024-04-14 02:12:44 +0100

    created the player and its playlist

--- src/Pages/Training/workoutPlayer.js
+++ src/Pages/Training/workoutPlayer.js
@@ -1,11 +1,19 @@
 import React,{useState} from 'react'
+import { Link,useParams,Outlet } from 'react-router-dom'
+
 
 const WorkoutPlayer = () => {
+  const {workout} = useParams()
+
+
     const [videoLoading, setVideoLoading] = useState(false)
-    const loading =videoLoading ?  <div>...loading</div> : <video></video>
+    const content = videoLoading ?  <div>...loading</div> : <div className='bg-green-500 w-[100%] h-[65vh]'>
+        {workout}
+        {/* <video></video> */}
+        </div> 
   return (
     <div>
-
+        {content}
     </div>
   )
 }
