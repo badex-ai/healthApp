57a4a4efabe708230b824835f91eae8792fe9712
badex-ai <Badamasi.link@gmail.com>
2024-04-28 22:21:59 +0100

    chore(trainers):added the mock images for the trainer

--- src/Pages/Trainers/trainersPage.js
+++ src/Pages/Trainers/trainersPage.js
@@ -2,8 +2,26 @@ import React from 'react'
 import Header from '../../components/header'
 import Search from '../../components/search'
 import Trainers from '../../components/layout/trainers'
+import maleTrainer1 from "../../assets/images/maleTrainer1.jpg"
+import maleTrainer2 from "../../assets/images/maleTrainer2.jpg"
+import femaleTrainer1 from "../../assets/images/femaleTrainer1.jpg"
+import femaleTrainer2 from "../../assets/images/femaleTrainer2.jpg"
 
 const TrainersPage = () => {
+
+ const trainersSample =[
+  {name: "Tunde", weight : 85, rating : 5, price: 1000, experience: 12, videoLink: '',img:maleTrainer1, sex: "Male" },
+  {name: "Bunmi", weight : 85, rating : 5, price: 1000, experience: 12, videoLink: '',img:femaleTrainer1, sex: "Female" },
+  {name: "Sesan", weight : 85, rating : 5, price: 1000, experience: 12, videoLink: '',img:maleTrainer2, sex: "Male" },
+  {name: "kehinde", weight : 65, rating : 5, price: 1000, experience: 12, videoLink: '',img:maleTrainer2, sex: "Male" },
+  {name: "Bisola", weight : 85, rating : 5, price: 1000, experience: 12, videoLink: '',img:femaleTrainer2, sex: "Female" },
+  {name: "Shane", weight : 85, rating : 5, price: 1000, experience: 12, videoLink: '', sex: "Trans" },
+ ]
+
+ const trainersDiv = trainersSample.map((trainer)=>(
+  <Trainers name={trainer.name} img={trainer.img} weight={trainer.weight} sex={trainer.sex}></Trainers>
+
+ ))
   return (
     <div>
         <Header
@@ -15,15 +33,7 @@ const TrainersPage = () => {
 			></Header>
         TrainersPage
         <div className='grid grid-cols-3 gap-16'>
-          <Trainers></Trainers>
-          <Trainers></Trainers>
-          <Trainers></Trainers>
-          <Trainers></Trainers>
-          <Trainers></Trainers>
-          <Trainers></Trainers>
-          <Trainers></Trainers>
-          <Trainers></Trainers>
-          <Trainers></Trainers>
+        {trainersDiv}
         </div>
         </div>
         
--- src/components/layout/trainers.js
+++ src/components/layout/trainers.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Trainers = () => {
+const Trainers = ({name,weight,sex,img}) => {
   return (
-    <div className='w-[28rem] h-[40rem] bg-green-500'>trainers</div>
+    <Link to={"./name"} className=' relative w-[28rem] h-[40rem] bg-green-500'>
+      <img src={img} alt="" />
+      <div className='absolute bottom-12 right-8 bg-yellow-800 color'>
+        <div className='font-extrabold text-[3.5rem] uppercase'>{name}</div>
+        <div className='font-bold text-bg '>{weight}</div>
+        <div className='font-bold text-bg capitalize'>{sex}</div>
+      </div>
+    </Link>
   )
 }
 
