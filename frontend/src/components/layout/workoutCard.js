20f50bd70fdb07b9d400e50f769c51c224efd898
badex-ai <Badamasi.link@gmail.com>
2024-06-08 22:03:13 +0100

    added the husky file and the training and an image in the training card

--- frontend/src/components/layout/workoutCard.js
+++ frontend/src/components/layout/workoutCard.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link, Outlet, useParams, Navigate } from "react-router-dom";
+import upperBodyImage from '../../assets/images/upperBody.jpg';
+
 
 
 const WorkoutCard = ({name,imgSrc}) => {
   return (
-    <Link to={`./${name}`} className='w-[50rem] h-[30rem] bg-yellow-500'>
-        <img src="" alt="" />
-        <div>{name}</div>
+    <Link to={`./${name}`} className='w-[50rem] h-[30rem] bg-yellow-500 relative'>
+        <img src={upperBodyImage} alt="" />
+        <div className='text-[2rem] font-bold z-1 absolute left-4 bottom-8'>{name}</div>
     </Link>
   )
 }
